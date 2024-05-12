c0955afa9673840b4ec3515e6be5bbafb8917033
Dezertir098 <166860232+Dezertir098@users.noreply.github.com>
2024-05-12 20:05:02 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+const search = document.getElementById("search");
+const window = document.getElementById("window");
+
+search.addEventListener('click', function() {
+    alert('Кнопка была нажата!');
+    });
